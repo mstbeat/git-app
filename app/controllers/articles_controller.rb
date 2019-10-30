afd366baf1d44e7da26002304f1f8b9f8db918cc
mstbeat <mrbrightside82@ymail.com>
2019-10-30 17:25:38 +0900

    fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,43 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
+<<<<<<< Updated upstream
   # GET /articles
   # GET /articles.json
+=======
+>>>>>>> Stashed changes
   def index
     @articles = Article.all
   end
 
+<<<<<<< Updated upstream
   # GET /articles/1
   # GET /articles/1.json
   def show
   end
 
   # GET /articles/new
+=======
+  def show
+  end
+
+>>>>>>> Stashed changes
   def new
     @article = Article.new
   end
 
+<<<<<<< Updated upstream
   # GET /articles/1/edit
   def edit
   end
 
   # POST /articles
   # POST /articles.json
+=======
+  def edit
+  end
+
+>>>>>>> Stashed changes
   def create
     @article = Article.new(article_params)
 
@@ -37,8 +52,11 @@ class ArticlesController < ApplicationController
     end
   end
 
+<<<<<<< Updated upstream
   # PATCH/PUT /articles/1
   # PATCH/PUT /articles/1.json
+=======
+>>>>>>> Stashed changes
   def update
     respond_to do |format|
       if @article.update(article_params)
@@ -51,8 +69,11 @@ class ArticlesController < ApplicationController
     end
   end
 
+<<<<<<< Updated upstream
   # DELETE /articles/1
   # DELETE /articles/1.json
+=======
+>>>>>>> Stashed changes
   def destroy
     @article.destroy
     respond_to do |format|
@@ -62,12 +83,18 @@ class ArticlesController < ApplicationController
   end
 
   private
+<<<<<<< Updated upstream
     # Use callbacks to share common setup or constraints between actions.
+=======
+>>>>>>> Stashed changes
     def set_article
       @article = Article.find(params[:id])
     end
 
+<<<<<<< Updated upstream
     # Never trust parameters from the scary internet, only allow the white list through.
+=======
+>>>>>>> Stashed changes
     def article_params
       params.require(:article).permit(:title, :author, :text)
     end
